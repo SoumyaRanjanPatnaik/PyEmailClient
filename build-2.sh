bf7c74a91a38566d9c4149260234997e87ff2d2d
Soumya Ranjan Patnaik <soumyaranjan1812@google.com>
2021-06-08 21:22:55 +0530

    Removed build.sh

--- build-2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-touch token_gmail_v1.pickle
-python -m eel Main.py Static --add-data "client_secret.json:." --add-data "token_gmail_v1.pickle:." --noconsole
-
-rm ./dist/Main/token_gmail_v1.pickle
-mv token_gmail_v1.pickle.old token_gmail_v1.pickle
