515197009ad026495d14a6a396fbfb0b228cec3a
Soumya Ranjan Patnaik <soumyaranjan1812@google.com>
2021-06-20 03:04:54 +0530

    Moved subject to the top and modified fonts

--- Static/js/readmail.js
+++ Static/js/readmail.js
@@ -9,8 +9,7 @@ async function readmail(id){
 
 	from_html = "<h3 id = 'from'><strong>From: </strong>" +"<span>"+ sender+"</span>"+"</h3>";
 	to_html = "<h3 id = 'to'><strong>To: </strong>" +"<span>"+ reciever+"</span>"+"</h3>";
-	subject_html = "<h3 id = 'to'><strong>Subject: </strong>" +"<span>"+ subject+"</span>"+"</h3>";
+	subject_html = "<h1 id = 'subject'><span>"+ subject+"</span>"+"</h1>";
 	body_html = "<iframe id = 'mail-body' style='display=block;' srcdoc = '"+body+"'></iframe>";
-	
-	pane.innerHTML = from_html + to_html +subject_html+"<br><br>"+ body_html;
+	pane.innerHTML = subject_html+"<br>" + from_html + to_html +"<br><br>"+ body_html;
 }
