e7ee5f31755224e47812211b52e51aa6e8065d83
DivyanshuTirkey <76056789+DivyanshuTirkey@users.noreply.github.com>
2021-06-23 22:42:59 +0530

    Update labels.js

--- Static/js/labels.js
+++ Static/js/labels.js
@@ -1,17 +1,9 @@
-async function getlabels(){
+async function getlabels() {
     let labels = await eel.mail_labels()();
-    for (let index = 0; index < labels['imp'].length; index++) {
-        if(labels['imp'][index]=='INBOX'){
-            document.getElementById('label').innerHTML += "<li id='"+labels['imp'][index]+"' class='active' onclick=load_label('INBOX')><a href = '#'><span>"+labels['imp'][index]+"</span></a></li>"
-            continue;
+    for (let index = 0; index < labels.length; index++) {
+        if (labels[index].type === 'user') {
+            document.getElementById('label').innerHTML += "<li id='" + labels[index].id + "' onclick='load_label(this.id)'><a href = '#'><i class='fas fa-folder-open'></i><span>" + labels[index].name + "</span></a></li>"
         }
-        document.getElementById('label').innerHTML += "<li id='"+labels['imp'][index]+`' onclick='load_label(this.id)'><a href = '#'><span>`+labels['imp'][index]+"</span></a></li>"
-    }
-    if (labels['other'].length>0){
-        document.getElementById('label').innerHTML += "<li id = 'other'><a href = '#'><span>Others</span></a><div class = 'sub-menu-2'><ul id = 'other_label'></ul></div></li>"
-    }
-    for (let index = 0; index < labels['other'].length; index++) {
-        document.getElementById('other_label').innerHTML += "<li><a href = '#'>"+labels['other'][index]+"</a></li>"
     }
 }
 
